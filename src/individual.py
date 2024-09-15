0411c02ac32b8756a636e06eaf0e4d31a6b2e60d
Soulergonote <110846563+Soulergonote@users.noreply.github.com>
2024-09-15 13:11:14 +0800

    individual fully tested

--- src/individual.py
+++ src/individual.py
@@ -1,14 +1,45 @@
+from random import random, randint, uniform
+from src.utils import GenesType
+
 # Class Individual
 class Individual:
-    def __init__(self, genes):
-        self.genes = genes
-        self.fitness = 0
+    def __init__(self, genes_type, genes_max_value=1, genes_length=10, genes=None):
+        # If no genes provided, generate them
+        if genes is None:
+            if genes_type == GenesType.INTEGER:
+                self.genes = [randint(0,genes_max_value) for _ in range(genes_length)]
+            if genes_type == GenesType.FLOAT:
+                self.genes = [uniform(0,genes_max_value) for _ in range(genes_length)] 
+        else:
+            self.genes = genes[::]
+            genes_length = len(genes)
+        self.genes_length = genes_length
+        self.genes_type = genes_type
+        self.genes_max_value = genes_max_value
+        self.calculate_fitness()
 
     # Fitness calculation (must be customized for the problem)
     def calculate_fitness(self):
         # Example: maximize the sum of genes (this should be modified according to the problem)
         self.fitness = sum(self.genes)
 
+    def crossover(self, partner):
+        # Single-point crossover
+        crossover_point = randint(1, self.genes_length - 1)
+        child1_genes = self.genes[:crossover_point] + partner.genes[crossover_point:]
+        child2_genes = partner.genes[:crossover_point] + self.genes[crossover_point:]
+        return Individual(genes_type=self.genes_type, genes=child1_genes, genes_max_value=self.genes_max_value), Individual(genes_type=self.genes_type, genes=child2_genes, genes_max_value=self.genes_max_value)
+    
+    def mutate(self, mutation_rate):
+        # Mutate based on mutation rate
+        for i in range(self.genes_length):
+            if random() < mutation_rate:
+                if self.genes_type == GenesType.INTEGER:
+                    self.genes[i] = randint(0, self.genes_max_value)
+                if self.genes_type == GenesType.FLOAT:
+                    self.genes[i] = uniform(0, self.genes_max_value)
+        self.calculate_fitness()
+    
     # String representation for debugging
     def __repr__(self):
         return f'Individual(genes={self.genes}, fitness={self.fitness})'
--- tests/test_individual.py
+++ tests/test_individual.py
@@ -6,3 +6,32 @@ def test_init():
     indiv_int = Individual(genes_type=GenesType.INTEGER)
     indiv_float = Individual(genes_type=GenesType.FLOAT)
     assert (indiv_int is not None) and (indiv_float is not None)
+    assert True
+
+def test_attributes():
+    indiv1 = Individual(genes_type=GenesType.INTEGER, genes_max_value=10, genes_length=5)
+    assert max(indiv1.genes) <= indiv1.genes_max_value
+    assert len(indiv1.genes) == indiv1.genes_length
+    
+    indiv2 = Individual(genes_type=GenesType.INTEGER,genes=[0,1,2,3], genes_max_value=3)
+    assert indiv2.fitness is not None
+
+def test_crossover():
+    parent1 = Individual(genes_type=GenesType.INTEGER)
+    parent2 = Individual(genes_type=GenesType.INTEGER)
+    child1, child2 = parent1.crossover(parent2)
+    child3, child4 = parent2.crossover(parent1)
+    assert child1.genes_type == parent1.genes_type
+    assert child1.genes_length == parent1.genes_length
+    assert child1.genes_max_value == parent1.genes_max_value
+
+def test_mutate():
+    original_genes = [0 for _ in range(10)]
+    indiv = Individual(genes_type=GenesType.INTEGER, genes=original_genes, genes_max_value=1)
+    indiv.mutate(0)
+    assert indiv.genes == original_genes
+    indiv.mutate(1)
+    assert indiv.genes != original_genes
+    assert max(indiv.genes) <= 1
+    assert indiv.genes_length == len(original_genes)
+    assert indiv.genes_max_value == 1
